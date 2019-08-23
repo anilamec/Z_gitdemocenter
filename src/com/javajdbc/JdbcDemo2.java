22b6a6664566a40fdddbd821b27acc0b9ddd86c6
anil lamichhane <anilameec@gmail.com>
2019-08-22 22:14:04 -0500

    a

--- src/com/javajdbc/JdbcDemo2.java
+++ src/com/javajdbc/JdbcDemo2.java
@@ -3,7 +3,7 @@ package com.javajdbc;
 public class JdbcDemo2 {
 
 	public static void main(String[] args) {
-		System.out.println("I am from the tester/anilameec");
+		System.out.println("I am from the newtester/anilameec");
 
 	}
 
