ffa5d5dc9a4a6aeb593a2f5db383ca1fe4bb8951
anil lamichhane <anilameec@gmail.com>
2019-08-24 15:33:21 -0500

    aa commit and modified

--- src/com/javajdbc/JdbcDemo2.java
+++ src/com/javajdbc/JdbcDemo2.java
@@ -4,7 +4,7 @@ public class JdbcDemo2 {
 
 	public static void main(String[] args) {
 		System.out.println("I am from the tester/anilameec");
-
+       System.out.println("I am from the tester/anilameec2");
 	}
 
 }
